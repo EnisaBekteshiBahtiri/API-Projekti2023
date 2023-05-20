d2b0f61e084553206533c9a13dcc387de3f9a4b2
EnisaBekteshiBahtiri <enisabekteshi97@gmail.com>
2023-05-20 18:15:11 +0200

    Add project files.

---
+++ ASP.NET Core 6.0 API/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using ASP.NET_Core_6._0_API.Data;
+using ASP.NET_Core_6._0_API.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ASP.NET_Core_6._0_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public UserController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(CreateUserDTO model)
+        {
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Je regjistruar me sukses");
+            }
+            return BadRequest("Deshtim");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(CreateUserDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo,
+                    UserDetails = new
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    }
+                });
+            }
+
+            return Unauthorized();
+
+        }
+    }
+}
